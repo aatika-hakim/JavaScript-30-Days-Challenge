69610512aa4031c7034f6f9c78e30ffe19090816
Aatika hakim <aatikahakim90@gmail.com>
2024-07-14 12:03:20 +0500

    arithmetic operators

--- Day_2.js
+++ Day_2.js
@@ -1,2 +1,31 @@
 /********************** Operators *********************/
 
+// Arithmetic Operators
+/* Task 1: Write a program to add two numbers and log the result to the console */
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 + num2); // 30
+
+
+/* Task 2: Write a program to subtract two numbers and log the result to the console */
+
+console.log(num2 - num1); // 10
+
+/* Task 3: Write a program to multiply two numbers and log the result to the console */
+
+console.log(num1 * num2); // 200
+
+/* Task 4: Write a program to divide two numbers and log the result to the console */
+
+console.log(num2 / num1); // 2
+
+/* Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console */
+
+console.log(num2 % num1); // 0
+
+
+
+
+
