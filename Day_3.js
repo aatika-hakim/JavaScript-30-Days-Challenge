2dc6c15a11dfff3dbc98bc468f8a53d241db8df8
Aatika hakim <aatikahakim90@gmail.com>
2024-07-15 11:07:41 +0500

    if-else statements

--- Day_3.js
+++ Day_3.js
@@ -6,6 +6,30 @@
     3. conditional ternary operator
 */
 
+/* Task 1: Write a program to check if a number is positive,negative, or zero,and the log the results to the console. */
+
+let num = 5;
+// num = 0;
+// num = -5;
+
+if (num > 0) {
+    console.log("The number is positive");
+} else if (num < 0) {
+    console.log("The number is negative");
+} else {
+    console.log("The number is zero");
+}
+
+/* Task 2: Write a program to check if a person is eligible to vot (age >= 18) and log the result to the console. */
+
+let age = 18;
+
+if (age >= 18) {
+    console.log("The person is eligible to vote");
+}
+else {
+    console.log("The person is not eligible to vote");
+}
 
 
 
