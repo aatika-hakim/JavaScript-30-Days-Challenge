cd66f9d5bb462613425fab4c652cb1a9c02e0085
Aatika hakim <aatikahakim90@gmail.com>
2024-07-16 11:26:34 +0500

    switch case

--- Day_3.js
+++ Day_3.js
@@ -67,6 +67,33 @@ switch (day) {
     case 7:
         console.log("Saturday");
         break;
+    default:
+            console.log("Invalid day");
+}
+
+/* Task 5: Write a program that uses a switch case to assign a grade ('A','B', 'C', 'D', 'F') based on a score and log the grade to the console.*/
+
+let score =90;
+let grade;
+
+switch(true){
+    case(score >=90 || score <= 100):
+    grade = "A";
+    break;
+    case(score >=80 || score <= 89):
+    grade = "B";
+    break;
+    case(score >=70 || score <= 79):
+    grade = "C";
+    break;
+    case(score >=60 || score <= 69):
+    grade = "D";
+    break;
+    case(score >=0 || score <= 59):
+    grade = "F";
+    break;
+    default:
+        grade = "Invalid Score";
 }
 
 
