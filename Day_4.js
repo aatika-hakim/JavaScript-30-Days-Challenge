d4b9793b195ab95a92abfd10ef81423bc9bf1c62
Aatika hakim <aatikahakim90@gmail.com>
2024-07-16 12:19:00 +0500

    for loop

--- Day_4.js
+++ Day_4.js
@@ -8,3 +8,19 @@
     5. loop control statements
 */
 
+
+/* Task 1: Write a program to pront numbers from 1 to 10 using for loop. */
+
+for(let count = 0; count <=10; count++){
+    console.log(count);
+}
+
+/* Task 2: Write a program to print multiplication table of 5 using for loop. */
+
+let i =5;
+
+    for (let j = 0; j <= 10; j++){
+        console.log(i, " ", "*", " ", j, "=", " ", i*j)
+    }
+
+
