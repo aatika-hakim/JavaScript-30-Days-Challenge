abc053a2c0faaf3be9dc8739dad1922cd2cddc5b
Aatika hakim <aatikahakim90@gmail.com>
2024-07-16 11:46:54 +0500

    conditional ternary operator

--- Day_3.js
+++ Day_3.js
@@ -67,33 +67,43 @@ switch (day) {
     case 7:
         console.log("Saturday");
         break;
-    default:
-            console.log("Invalid day");
 }
 
 /* Task 5: Write a program that uses a switch case to assign a grade ('A','B', 'C', 'D', 'F') based on a score and log the grade to the console.*/
 
-let score =90;
+let score = 8;
 let grade;
 
-switch(true){
-    case(score >=90 || score <= 100):
-    grade = "A";
-    break;
-    case(score >=80 || score <= 89):
-    grade = "B";
-    break;
-    case(score >=70 || score <= 79):
-    grade = "C";
-    break;
-    case(score >=60 || score <= 69):
-    grade = "D";
-    break;
-    case(score >=0 || score <= 59):
-    grade = "F";
-    break;
+switch (true) {
+    case (score >= 90 && score <= 100):
+        grade = 'A';
+        break;
+    case (score >= 80 && score < 90):
+        grade = 'B';
+        break;
+    case (score >= 70 && score < 80):
+        grade = 'C';
+        break;
+    case (score >= 60 && score < 70):
+        grade = 'D';
+        break;
+    case (score >= 0 && score < 60):
+        grade = 'F';
+        break;
     default:
-        grade = "Invalid Score";
+        grade = "Invalid score";
 }
+
+console.log(grade);
+
+/* Task 6: Write a program that uses the ternary operator to check if a number is even or odd and log the result to the console. */ 
+
+let NumberToCheck = 45;
+
+let results = NumberToCheck % 2 == 0 ? "Even" : "Odd";
+console.log(results);
+
+
+
 
 
