208d61e955e72ccce9efbd1f664b9407bd728bba
Aatika hakim <aatikahakim90@gmail.com>
2024-07-17 12:20:19 +0500

    functions return statements

--- Day_5.js
+++ Day_5.js
@@ -23,41 +23,44 @@ isEvenOrOdd(2); // The number is even
 /* Task 2: Write s function to calculate the square of a number and return the result.*/
 
 function calc_Square(num){
-    console.log("The square of", num, "=", num * num);
+    return num * num;
 }
 
-calc_Square(12);
+console.log("The square of the number =", calc_Square(12));
+
 
 /* Task 3: Write a function expression to find the maximum of two numbers and log the result to the console. */
 
 function findMaximum(a, b) {
-    console.log( a > b ? a : b); // ternary operator
+    return  a > b ? a : b; // ternary operator
 }
 
-findMaximum(2,6);
-findMaximum(10,8);
+console.log(findMaximum(2,6));
 
-/* Task 4: Write a function expression to concatenate two strings and log the results to the console.*/
+/* Task 4: Write a function expression to concatenate two strings and return the results*/
 
 function concatStrings(str1, str2){
-    console.log(str1 + str2);
+    return str1 + str2
     }
 
-concatStrings("str", "ing");
+    console.log(concatStrings("str", "ing"));
 
 
-/* Task 5: Write an arrow function to calculate the sum of the two numbers and log the result to the console. */
+/* Task 5: Write an arrow function to calculate the sum of the two numbers and  return the result */
 
-const Sum = (a, b) => console.log("Sum of", a, "and", b, "=", a + b);
+const Sum = (a, b) =>  { return a + b};
 
-Sum(2, 6);
+console.log("Sum =" , Sum(2,6)); 
 
 
-/* Task 6:  Write an arrow function to check if a string contains a specific character and log the result to the console.*/
 
-const check_Char = (str, char) => console.log(str.includes(char) ? "Yes" : "No"); // ternary operator
+/* Task 6:  Write an arrow function to check if a string contains a specific character and return the result*/
 
-check_Char("Hello World","l");
+const check_Char = (str, char) => { return str.includes(char) ? "Yes" : "No"}; // ternary operator
+
+console.log(check_Char("Hello World","l"));
+
+
 
 
 
