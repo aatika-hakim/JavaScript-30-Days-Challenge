9fe09f94fa583b30774b43b6e54623348bcfbcda
Aatika hakim <aatikahakim90@gmail.com>
2024-07-17 11:13:41 +0500

    functions

---
+++ Day_5.js
@@ -0,0 +1,9 @@
+/*---------------- Functions ------------------*/ 
+
+/*
+1. Function Declaration
+2. Function Expression
+3. Arrow Function
+4. Function Parameters & Default values
+5. Higher-Order Functions
+*/ 
