c817ccd9a4d1c5abef733ca7d2f11034d5d8e807
Aatika hakim <aatikahakim90@gmail.com>
2024-07-18 11:18:31 +0500

    Array creation & access

--- Day_6.js
+++ Day_6.js
@@ -8,4 +8,18 @@
     * 5. Multi-dimensional Arrays
 */ 
 
+//************** 1. Array Creation & Access ***************//
+
+/* Task 1: Create an array of numbers from 1 to 5 and log the result to the console. */
+
+let numberArray = [1, 2, 3, 4, 5];
+console.log(numberArray); // [1, 2, 3, 4, 5]
+
+/* Task 2:  Access the first and last elements of the array and log them to the console.*/
+// array index start from 0
+
+console.log(numberArray[0]); // 1  first element
+console.log(numberArray[4]); // 5  last element
+
+
 
