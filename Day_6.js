e7c28530ecc3226ee9da23e13b6517c81912d0b6
Aatika hakim <aatikahakim90@gmail.com>
2024-07-18 11:19:17 +0500

    push, pop, shift, unshift methods in array

--- Day_6.js
+++ Day_6.js
@@ -22,4 +22,29 @@ console.log(numberArray[0]); // 1  first element
 console.log(numberArray[4]); // 5  last element
 
 
+//************** 2. Array Methods(Basic) ***************//
+
+/* Task 3: Use the push method to add a new number to the end of the array and log the updated array to the console.*/
+
+numberArray.push(6); // number to add
+console.log(numberArray); // [ 1, 2, 3, 4, 5, 6 ]
+
+/* Task 4: Use the pop method to remove the last element from the array and log the updated array to the console.*/
+
+
+numberArray.pop(6); // number to remove
+console.log(numberArray); // [ 1, 2, 3, 4, 5]
+
+/* Task 5:  Use the shift method to remove the first element from the array and log the updated array to the console.*/ 
+
+numberArray.shift([0]); // index of the number to be removed
+console.log(numberArray); // [ 2, 3, 4, 5]
+
+/* Task 6:  Use the unshift method to add the new number at the beginning of the array and log the updated array to the console.*/ 
+
+// original array :[ 2, 3, 4, 5]
+numberArray.unshift(0); 
+console.log(numberArray); // updated array: [ 0, 2, 3, 4, 5 ]
+
+
 
