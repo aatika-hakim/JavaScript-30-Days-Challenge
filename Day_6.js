c228b4a48cf070637d71b1f7bd442cb77f2ba9e7
Aatika hakim <aatikahakim90@gmail.com>
2024-07-18 10:35:56 +0500

    Arrays in javascript

---
+++ Day_6.js
@@ -0,0 +1,11 @@
+//************** Arrays ***************//
+
+/* 
+    * 1. Array Creation & Access
+    * 2. Array Methods(Basic)
+    * 3. Array Methods(Intermediate)
+    * 4. Array Iteration
+    * 5. Multi-dimensional Arrays
+*/ 
+
+
