2158efddeaa48f58eeace7bf7fa6906f37ee313b
Aatika hakim <aatikahakim90@gmail.com>
2024-07-20 10:27:33 +0500

    object exercises

--- Day_7.js
+++ Day_7.js
@@ -1,1 +1,12 @@
 // ****************  Objects **************** //
+
+/*
+Activities: 
+
+1. Create an object & Access
+2. Object Methods
+3. Nested Objects
+4. this keyword
+5. Object Iteration
+
+*/ 
