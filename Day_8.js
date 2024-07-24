591a2438600dfa25a2daf412b6aba68c5cb59888
Aatika hakim <aatikahakim90@gmail.com>
2024-07-24 11:25:42 +0500

    enhanced object literals

--- Day_8.js
+++ Day_8.js
@@ -9,12 +9,11 @@ Activities:
 5. Default Parameters
 6. enhanced Object Literals
 
-*/ 
-
+*/
 
 // ****************  Template Literals **************** //
 
-/* Task 1: Use template literals to create a string that includes variables for a person's name and age, and log the string to the console. */ 
+/* Task 1: Use template literals to create a string that includes variables for a person's name and age, and log the string to the console. */
 
 let Name = "Lily";
 let age = 20;
@@ -27,6 +26,8 @@ My Name is ${Name}.
 I am ${age} years old.
 `;
 console.log(multiLineString);
+
+
 
 // ****************  Destructuring **************** //
 
@@ -42,17 +43,15 @@ console.log(elements);
 let book = {
     title: "JavaScript",
     author: "David",
-    year: 2019
-    };
+    year: 2019,
+};
 // 1st method:
-    console.log(book.title);
-    console.log(book.author);
+console.log(book.title);
+console.log(book.author);
 
 // 2nd method:
-    // let {title, author} = book;
-    // console.log(`"${title}" by "${author}"`);
-
-
+// let {title, author} = book;
+// console.log(`"${title}" by "${author}"`);
 
 // ********  Spread Operator & rest Operators ******** //
 
@@ -64,7 +63,6 @@ let newArray = [...existingArray, 6, 7, 8, 9, 10];
 console.log(newArray);
 
 /* Task 6: Use rest operator in a function to accept an arbitrary number of arguments sum them and return the result.*/
-
 
 function sum(...args) {
     let sum = 0;
@@ -80,20 +78,38 @@ console.log(sum(1, 2, 3, 4, 5));
 
 // ********  Default Parameters ******** //
 
-/* Task 7: Write a function that takes two parameters and returns their product, with second parameter having default value of 1. Log the result  of calling this function with and without the second parameter.*/ 
+/* Task 7: Write a function that takes two parameters and returns their product, with second parameter having default value of 1. Log the result  of calling this function with and without the second parameter.*/
 
-function product(a, b = 1){
+function product(a, b = 1) {
     return a * b;
 }
 
 console.log(product(3)); //without second parameter
-console.log(product(5, 9));  //with second parameter
+console.log(product(5, 9)); //with second parameter
 
 
 
+// ********  Enhanced Object Literals ******** //
 
+/* Task 8: Use enhanced object literals and create an object with methods and properties and log the object to the console.*/
 
+let Book = {
+    title: "JS",
+    author: "David",
 
+    getBookInfo: function () {
+        return `The book "${this.title}" was written by "${this.author}".`;
+    },
+};
+console.log(Book.getBookInfo());
 
+/* Task 9: Create an object with computed property names based on variables and log the object to the console.*/
 
+let bookTitle = "JS";
+let bookAuthor = "David";
+let BookInfo = {
+    [bookTitle]: bookAuthor,
+    [bookAuthor]: bookTitle,
+    };
+    console.log(BookInfo);
 
