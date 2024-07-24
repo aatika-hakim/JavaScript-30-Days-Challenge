3ff7919a799865723846bb99c3ccdcffab7bb54f
Aatika hakim <aatikahakim90@gmail.com>
2024-07-24 09:34:59 +0500

    Template literals

--- Day_8.js
+++ Day_8.js
@@ -12,3 +12,18 @@ Activities:
 */ 
 
 
+// ****************  Template Literals **************** //
+
+/* Task 1: Use template literals to create a string that includes variables for a person's name and age, and log the string to the console. */ 
+
+let Name = "Lily";
+let age = 20;
+console.log(`My Name is ${Name} and I am ${age} years old.`);
+
+/* Task 2: Create a multi-line string using template literals and log it to the console.*/
+
+let multiLineString = `
+My Name is ${Name}.
+I am ${age} years old.
+`;
+console.log(multiLineString);
