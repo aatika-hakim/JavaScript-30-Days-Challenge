8474e4fd5b1c7348ab31c40c071fd2be37e7008c
Aatika hakim <aatikahakim90@gmail.com>
2024-07-24 09:34:36 +0500

    Es6+ Features

---
+++ Day_8.js
@@ -0,0 +1,14 @@
+// ****************  ES6+ Features **************** //
+
+/*
+Activities:
+
+1. Template Literals
+3. Destructuring
+4. Spread Operator & rest Operators
+5. Default Parameters
+6. enhanced Object Literals
+
+*/ 
+
+
