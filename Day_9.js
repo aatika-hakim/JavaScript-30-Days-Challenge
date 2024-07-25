21be1ae4dbf8bde048e36600b7f895abc22fd8c4
Aatika hakim <aatikahakim90@gmail.com>
2024-07-25 10:11:02 +0500

    Selecting & Manipulating Elements

---
+++ Day_9.js
@@ -0,0 +1,21 @@
+// ****************  DOM MANIPULATION **************** //
+
+/* 
+Activities:
+1. Selecting & Manipulating Elements
+2. Creating & Appending Elements
+3. Removing Elements
+4. Modifying Attributes and classes
+5. Event Handling
+*/
+
+// ********  Selecting & Manipulating Elements  ********** //
+
+/* Task 1: Select an HTML element by its ID and change its text content.*/
+
+document.getElementById("task1").innerHTML = "'JAVASCRIPT' 30 DAYS CHALLANGE BY 'CHAICODE'";
+
+/* Task 2: Select an HTML element by its class and change its background color. */
+
+document.getElementsByClassName("task2")[0].style.backgroundColor = "red";
+
