5c53a534fe11c449b368b7af6ed2008cb3b9171e
Aatika hakim <aatikahakim90@gmail.com>
2024-07-26 11:08:20 +0500

    Modifying attributes & classes

--- Day_9.js
+++ Day_9.js
@@ -20,6 +20,7 @@ document.getElementById("task1").innerHTML = "'JAVASCRIPT' 30 DAYS CHALLANGE BY 
 document.getElementsByClassName("task2")[0].style.backgroundColor = "red";
 
 
+
 // ********  Creating & Appending Elements  ********** //
 
 /* Task 3:  Create a new div element with some text content and append it to the body.*/
@@ -36,6 +37,8 @@ newLi.innerHTML = "New List";
 var ul = document.getElementById("task4");
 ul.appendChild(newLi);
 
+
+
 // ********  Removing Elements  ********** //
 
 /* Task 5: Select an element and remove it from DOM.*/
@@ -46,3 +49,17 @@ document.getElementById("task5").remove();
 
 ul.removeChild(ul.lastChild); // remove last child of ul
 
+
+
+// ********  Modifying Attributes and classes  ********** //
+
+/* Task 7: Select an HTML element and change one of its attributes (e.g. src of an img tag) */
+
+document.getElementById("task7").src = "https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" // change src of img tag
+
+/* Task 8: Add and remove a CSS class to/from an HTML element. */
+
+document.getElementById("task7").classList.add("task8");
+document.getElementById("task7").classList.remove("task8"); // add and remove class from img tag
+
+ 
