9032ddb93976c431984bab9d98c032922d959847
Aatika hakim <aatikahakim90@gmail.com>
2024-07-29 11:22:17 +0500

    basic event handling

--- Day_10.js
+++ Day_10.js
@@ -1,6 +1,7 @@
 // *Event Handling //
 
 // *Activity 1: Basic Event Handling
+
 // * Task 1: Add a click event listener to a button that changes the text content of a paragraph.
 
 document.getElementsByTagName("button").addEventListener("click", function(){
@@ -8,7 +9,11 @@ document.getElementsByTagName("button").addEventListener("click", function(){
 });
 
 // * Task 2: Add a double-click event listener to an image that toggles its visibility.
-
-document.getElementsById("task2").addEventListener("dblclick", function(){
-    document.getElementsById("img").style.visibility = "hidden";
-});
+function show() {
+    var image = document.getElementById("img");
+    if (image.style.visibility === "hidden") {
+        image.style.visibility = "visible";
+    } else {
+        image.style.visibility = "hidden";
+    }
+}
