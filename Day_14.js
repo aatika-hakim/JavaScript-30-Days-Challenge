0a9b212c61208f3178a9209259bf39a4652ee846
Aatika hakim <aatikahakim90@gmail.com>
2024-08-12 10:45:41 +0500

    class Inheritance

--- Day_14.js
+++ Day_14.js
@@ -30,8 +30,39 @@ person.updateAge(21);
 console.log("Updated:", person.greeting()); 
 
 
+//* Activity 2: Class Inheritance
 
+// Task 3: Define a class Student that extends the Person class. Add a property studentId and a method to return the student ID. Create an instance of the Student class and log the student ID.
+class Student extends Person {
+    constructor(name, age, studentId) {
+        // Call the parent class constructor
+        super(name, age);
+        this.studentId = studentId;
+    }
+    
+    // Method to return the student ID
+    getStudentId() {
+        return `Student ID: ${this.studentId}`;
+    }
 
+//  Task 4: Override the greeting method in the Student class to include the student ID in the message. Log the overridden greeting message.
+
+       // Override the greeting method to include the student ID
+        greeting() {
+        // Call the parent class greeting method
+        let parent_Greeting = super.greeting();
+        // Append the student ID to the greeting message
+        return `${parent_Greeting} My student ID is ${this.studentId}.`;
+    }
+}
+
+// Create an instance of the Student class
+let student = new Student('LILY', 21, 'S-12345');
+
+console.log(student.getStudentId());
+
+// Task 4
+console.log(student.greeting());
 
 //* Activity 3: Static Methods and Properties
 
