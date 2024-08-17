c159f74f73a32291a5954c1324927c12871e58bc
Aatika hakim <aatikahakim90@gmail.com>
2024-08-17 10:34:00 +0500

    closures in js

---
+++ Day_15.js
@@ -0,0 +1,11 @@
+//* Closures: A closure is a function that has access to its own scope, the scope of its outer functions and the global scope.
+
+/* Activities
+1. Understanding Closures
+2. Creating Closures
+3. Closure in loops
+4. Module Pattern
+5. Memorization
+*/
+
+
