967fe373e984b9f6dca1c0bcdc4c29eddcb1caba
Aatika hakim <aatikahakim90@gmail.com>
2024-08-19 12:11:30 +0500

    Counter Closure Script

--- Day_15.js
+++ Day_15.js
@@ -26,3 +26,24 @@ inner();
 
 // task 2
 
+// Counter Closure Script
+function createCounter() {
+    let count = 0;
+    
+    return {
+        increment: function() {
+            count++;
+        },
+        getValue: function() {
+            return count;
+        }
+    };
+}
+
+const counter = createCounter();
+counter.increment();
+console.log(counter.getValue()); // 1
+counter.increment();
+console.log(counter.getValue()); // 2
+
+
