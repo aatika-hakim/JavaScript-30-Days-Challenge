0b9bb052f4e2af4f32177a0f12a2a2f7987b5c81
Aatika hakim <aatikahakim90@gmail.com>
2024-08-21 10:48:22 +0500

    recursive function to calculate fibonacci number

--- Day_16.js
+++ Day_16.js
@@ -1,6 +1,23 @@
-//* Recursion
+//* Recursion: the function calls itself
 
 //* Activities
 
 // Task 1
+function factorial(num){
+    if(num === 0){
+        return 1;
+    }
+    return num * factorial(num-1) 
+}
+
+console.log(factorial(20));
+
+// Task 2
+// recursive function to calculate fibonacci number. log the result for a few test cases.
+
+function fibonacci(number) {
+
+}
+
+
 
