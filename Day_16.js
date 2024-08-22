ff0f99cc028ec8d840bd1593df244907a3d3820f
Aatika hakim <aatikahakim90@gmail.com>
2024-08-22 13:51:26 +0500

    added

--- Day_16.js
+++ Day_16.js
@@ -3,21 +3,42 @@
 //* Activities
 
 // Task 1
-function factorial(num){
-    if(num === 0){
+function factorial(num) {
+    if (num === 0) {
         return 1;
     }
-    return num * factorial(num-1) 
+    return num * factorial(num - 1)
 }
 
 console.log(factorial(20));
 
 // Task 2
 // recursive function to calculate fibonacci number. log the result for a few test cases.
+// what is fibonacci number?
+// Fibonacci number is a number in the sequence where each number is the sum of the two preceding ones
+function fibonacci(n) {
+    if (n <= 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+// test cases
+console.log(fibonacci(10)); 
+console.log(fibonacci(20));
 
-function fibonacci(number) {
+// Recursion with arrays
+// Task 3
 
+function sumOfElements([array]){
+    if(array === ""){
+        return 0;
+    }
+    else {
+        return sumOfElements([array])
+    }
 }
 
-
-
+console.log(sumOfElements([12,34,45])); 
