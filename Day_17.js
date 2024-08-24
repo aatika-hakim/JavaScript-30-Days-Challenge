f8ea38718d41d30562b068a762fe0091b999fceb
Aatika hakim <aatikahakim90@gmail.com>
2024-08-24 12:19:12 +0500

    stack

--- Day_17.js
+++ Day_17.js
@@ -84,6 +84,8 @@ console.log("New Stack",stack)
 
 // Task 4: Use the Stack class to reverse a string by pushing all characters onto the stack and then popping them off.
 
+
+
 // Activity 3: Queue
 // Task 5: Implement a Queue class with methods enqueue (add element), dequeue (remove element), and front (view the first element).
 // Task 6: Use the Queue class to simulate a simple printer queue where print jobs are added to the queue and processed in order.
