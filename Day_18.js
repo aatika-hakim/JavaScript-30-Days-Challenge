bf93d0b1b51d49293046b5b2f35df710ce474f11
Aatika hakim <aatikahakim90@gmail.com>
2024-08-28 14:00:47 +0500

    tasks structure

--- Day_18.js
+++ Day_18.js
@@ -1,1 +1,48 @@
 // ***** Algorithms ***** //
+
+/*
+* Sorting Algorithms:
+    * Bubble sort
+    * Selection sort
+    * Quick sort
+*/
+
+//*Task 1: Bubble sort  
+//*Task 2: Selection sort
+//*Task 3: Quick sort
+
+
+/*
+* Searching Algorithms:
+    * Linear search
+    * Binary search
+*/
+
+//* Task 3: Linear Search
+//* Task 4: Binary Search
+
+
+/*
+* String Algorithms:
+    * occurrences of char
+    * longest substring
+*/
+
+//* Task 5: find occurrences of char
+//* Task 6:  find longest substring
+
+
+/*
+* Array Algorithms:
+    * rotate array by k-position
+    * merge arrays
+*/
+
+//* Task 7:  rotate array by k-position
+//* Task 8: merge arrays
+
+
+//* Dynamic Programming
+//* Task 9: Fibonacci sequence
+//* Task 10: knapsack. max value that can be obtained.
+
