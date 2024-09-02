d33aa66df374b6101573484cb71793518c987cd0
Aatika hakim <aatikahakim90@gmail.com>
2024-09-02 21:04:16 +0500

    Using localStorage

--- Day_20.js
+++ Day_20.js
@@ -1,7 +1,7 @@
 //* LocalStorage & SessionStorage
 
 
-// Activity 1: Understanding localStorage
+//* Activity 1: Understanding localStorage
 
 
 // Task 1: Save and Retrieve a String Value
@@ -17,3 +17,30 @@ const obj = { name: 'Ali', age: 22 };
 localStorage.setItem('obj', JSON.stringify(obj));
 const parsedObj = JSON.parse(localStorage.getItem('obj'));
 console.log(parsedObj);
+
+
+//* Activity 2: Using localStorage
+
+
+// Task 3: Simple Form with localStorage
+
+const form = document.querySelector('form');
+const nameField = document.querySelector('#name');
+const emailField = document.querySelector('#email');
+
+form.addEventListener('submit', () => {
+  localStorage.setItem('name', nameField.value);
+  localStorage.setItem('email', emailField.value);
+});
+
+window.addEventListener('load', () => {
+  nameField.value = localStorage.getItem('name') || '';
+  emailField.value = localStorage.getItem('email') || '';
+});
+
+
+// Task 4: Remove Item from localStorage
+
+console.log(localStorage.getItem('str'));
+localStorage.removeItem('str');
+console.log(localStorage.getItem('str'));
