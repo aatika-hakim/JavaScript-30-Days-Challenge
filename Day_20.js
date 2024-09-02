fe0f82c37712e7326229aaf3b80ef6e0847d0d5a
Aatika hakim <aatikahakim90@gmail.com>
2024-09-02 21:03:08 +0500

    Understanding localStorage

--- Day_20.js
+++ Day_20.js
@@ -1,2 +1,19 @@
 //* LocalStorage & SessionStorage
 
+
+// Activity 1: Understanding localStorage
+
+
+// Task 1: Save and Retrieve a String Value
+
+localStorage.setItem('str', 'Hello');
+const str = localStorage.getItem('str');
+console.log(str);
+
+
+// Task 2: Save and Retrieve an Object
+
+const obj = { name: 'Ali', age: 22 };
+localStorage.setItem('obj', JSON.stringify(obj));
+const parsedObj = JSON.parse(localStorage.getItem('obj'));
+console.log(parsedObj);
