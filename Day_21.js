928d656fc1d3f8d2dadbcb2130249773aca48987
Aatika hakim <aatikahakim90@gmail.com>
2024-09-03 20:29:10 +0500

    find Palindrome number

--- Day_21.js
+++ Day_21.js
@@ -34,3 +34,17 @@ console.log(reverseInteger(-123));  // -321
 console.log(reverseInteger(120));   // 21
 
 
+
+// Activity 3: Palindrome Number
+function isPalindrome(x) {
+    if (x < 0) return false;
+    const str = x.toString();
+    const reversedStr = str.split('').reverse().join('');
+    return str === reversedStr;
+}
+
+// Test cases
+console.log(isPalindrome(121));  // true
+console.log(isPalindrome(-121)); // false
+console.log(isPalindrome(10));   // false
+
