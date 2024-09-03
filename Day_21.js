896da6e4978a0a2b92f4c6d1178e2cfc9f2ad5cb
Aatika hakim <aatikahakim90@gmail.com>
2024-09-03 20:29:43 +0500

    Merge two sorted lists

--- Day_21.js
+++ Day_21.js
@@ -48,3 +48,43 @@ console.log(isPalindrome(121));  // true
 console.log(isPalindrome(-121)); // false
 console.log(isPalindrome(10));   // false
 
+
+
+// Activity 4: Merge Two Sorted Lists
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+function mergeTwoLists(l1, l2) {
+    const dummy = new ListNode();
+    let current = dummy;
+
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+    current.next = l1 !== null ? l1 : l2;
+    return dummy.next;
+}
+
+// Test cases
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+let mergedList = mergeTwoLists(list1, list2);
+
+while (mergedList !== null) {
+    console.log(mergedList.val);  // 1 -> 1 -> 2 -> 3 -> 4 -> 4
+    mergedList = mergedList.next;
+}
+
+
+
