79d4037626f020f70d2d9f5abaad93cae1559cdf
Aatika hakim <aatikahakim90@gmail.com>
2024-09-03 20:28:19 +0500

    Reverse integer

--- Day_21.js
+++ Day_21.js
@@ -18,3 +18,19 @@ console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
 console.log(twoSum([3, 2, 4], 6));      // [1, 2]
 console.log(twoSum([3, 3], 6));         // [0, 1]
 
+
+
+// Activity 2: Reverse Integer
+function reverseInteger(x) {
+    const sign = x < 0 ? -1 : 1;
+    const reversed = parseInt(Math.abs(x).toString().split('').reverse().join('')) * sign;
+    if (reversed < -(2 ** 31) || reversed > 2 ** 31 - 1) return 0;
+    return reversed;
+}
+
+// Test cases
+console.log(reverseInteger(123));   // 321
+console.log(reverseInteger(-123));  // -321
+console.log(reverseInteger(120));   // 21
+
+
